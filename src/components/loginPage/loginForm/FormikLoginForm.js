f0f56af6995eb72fd45ce90ed70605fbd7ff7a38
suark <suark.nairb@gmail.com>
2019-04-19 13:41:22 -0600

    Should be fixed now

---
+++ src/components/loginPage/loginForm/FormikLoginForm.js
@@ -0,0 +1,15 @@
+import { withFormik } from 'formik'
+import LoginForm from './LoginForm'
+import handleValidation from '../utils/validation'
+import handleSubmitLogin from '../utils/submit'
+
+const FormikLoginForm = withFormik({
+  mapPropsToValues: () => ({ email: '', password: ''}),
+
+  validate: handleValidation,
+  handleSubmit: handleSubmitLogin,
+
+  displayName: 'Prokeep Form',
+})(LoginForm)
+
+export default FormikLoginForm
